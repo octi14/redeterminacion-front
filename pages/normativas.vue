489029839e3b250887041e1b27461855e07f7f6d
octi14 <gonzalezoctavio5@gmail.com>
2023-02-08 10:55:02 -0300

    Upload normativas

--- pages/normativas.vue
+++ pages/normativas.vue
@@ -6,7 +6,13 @@
     <div class="container">
       <h4>Lista de normativas </h4>
       <div class="mt-5">
-        <a class="h5" style="color:blue" href="https://normas.gba.gob.ar/documentos/0X8LmqUa.html#:~:text=Decreto%20290%2F2021&text=ART%C3%8DCULO%201%C2%B0.,ART%C3%8DCULO%202%C2%B0." target="_blank"> Decreto 290/2021 </a>
+        <a class="h5" style="color:blue" href="https://normas.gba.gob.ar/documentos/0X8LmqUa.html#:~:text=Decreto%20290%2F2021&text=ART%C3%8DCULO%201%C2%B0.,ART%C3%8DCULO%202%C2%B0." target="_blank">- Decreto 290/2021 </a>
+      </div>
+      <div class="mt-5">
+        <a class="h5" style="color:blue" href="https://normas.gba.gob.ar/documentos/087dk9hZ.html" target="_blank">- Decreto 635/2021 </a>
+      </div>
+      <div class="mt-5">
+        <a class="h5" style="color:blue" href="http://servicios.infoleg.gob.ar/infolegInternet/anexos/280000-284999/280188/norma.htm" target="_blank">- Ley 27397 </a>
       </div>
     </div>
   </div>
